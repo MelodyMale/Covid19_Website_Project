e6c149c05d366ecbf39e786e4ec8400b619fa2d5
Chayutpong Promya <melodymale@gmail.com>
2020-05-11 20:35:58 +0700

    Initial app

--- Dashboard.py
+++ Dashboard.py
@@ -35,35 +35,34 @@ data_no_outlier = data[data["PROV_NAME"] != "Bangkok"]
 
 # Preparing map for ploting
 
-# with urlopen('https://raw.githubusercontent.com/apisit/thailand.json/master/thailandWithName.json') as response:
-#     thailand_map_data = json.load(response)
+with urlopen('https://raw.githubusercontent.com/apisit/thailand.json/master/thailandWithName.json') as response:
+    thailand_map_data = json.load(response)
 
-# for index, v in enumerate(thailand_map_data["features"]):
-#     if v["properties"]["name"] == "Bangkok Metropolis":
-#         thailand_map_data["features"][index]["properties"]["name"] = "Bangkok"
+for index, v in enumerate(thailand_map_data["features"]):
+    if v["properties"]["name"] == "Bangkok Metropolis":
+        thailand_map_data["features"][index]["properties"]["name"] = "Bangkok"
 
-# data_max = data_no_outlier["Count_of_no"].max()
+data_max = data_no_outlier["Count_of_no"].quantile(.95)
 
 
-# # Create the graph
+# Create the graph
 
-# fig = px.choropleth(data, geojson=thailand_map_data, color="Count_of_no",
-#                     locations="PROV_NAME", featureidkey="properties.name",
-#                     projection="mercator",
-#                     color_continuous_scale="Reds",
-#                     range_color = [0, data_max]
-#                    )
+fig = px.choropleth_mapbox(data, geojson=thailand_map_data, color="Count_of_no",
+                    locations="PROV_NAME", featureidkey="properties.name",
+                    color_continuous_scale="Reds",
+                    mapbox_style="carto-positron",
+                    zoom=5, center = {"lat": 13.7367, "lon": 100.5231},
+                    range_color = (1, data_max)
+                   )
 
-# fig.update_geos(fitbounds="locations", visible=False)
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-df = px.data.gapminder()
-fig = px.choropleth(df, locations="iso_alpha", color="lifeExp", hover_name="country", animation_frame="year", range_color=[20,80])
 
 # Create take the graph to the app
 
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    dcc.Graph(figure=fig,style={'height': '90vh', 'margin': '50px'})
 ])
 
 if __name__ == '__main__':
